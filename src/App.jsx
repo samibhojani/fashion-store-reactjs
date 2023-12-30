e1fe0d37d30abaef7957ccc64838b79d455ecda9
Sami Bhojani <65404958+samibhojani@users.noreply.github.com>
2023-12-30 11:24:14 +0500

    Created Home Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <>
     <Navbar />
+    <div id='home'>
+      <Home />
+    </div>
     </>
   )
 }
